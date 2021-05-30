ea8c0c6ef04935ad81bd3fb03ba6626117a25ea7
shaidsifat <shaidsifat55@gmail.com>
2021-05-31 05:14:41 +0600

    my app first commit

---
+++ NewsAggregator/news/views.py
@@ -0,0 +1,36 @@
+
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+
+# GEtting news from Times of India
+
+toi_r = requests.get("https://timesofindia.indiatimes.com/briefs")
+toi_soup = BeautifulSoup(toi_r.content, 'html5lib')
+
+toi_headings = toi_soup.find_all('h2')
+
+toi_headings = toi_headings[0:-13] # removing footers
+
+toi_news = []
+
+for th in toi_headings:
+    toi_news.append(th.text)
+
+
+
+#Getting news from Hindustan times
+
+ht_r = requests.get("https://www.prothomalo.com/topic/%E0%A6%AC%E0%A6%BF%E0%A6%B6%E0%A7%87%E0%A6%B7-%E0%A6%B8%E0%A6%82%E0%A6%AC%E0%A6%BE%E0%A6%A6")
+ht_soup = BeautifulSoup(ht_r.content, 'html5lib')
+ht_headings = ht_soup.find_all('h2')
+ht_headings = ht_headings[0:13]
+ht_news = []
+
+for hth in ht_headings:
+    ht_news.append(hth.text)
+
+
+
+def index(req):
+    return render(req, 'news/index.html', {'toi_news':toi_news, 'ht_news': ht_news})
